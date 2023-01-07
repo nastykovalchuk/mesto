d2e93988b47bffb3d99618f0079a29afe28ca717
nastykovalchuk <kovalchuk.anastasiya.m@yandex.ru>
2023-01-07 16:35:53 +0300

    добавила возможность менять масштаб картинки

--- script/index.js
+++ script/index.js
@@ -1,25 +1,28 @@
-let editButtonProfile = document.querySelector(".profile__edit-button");
-let addButtonProfile = document.querySelector(".profile__add-button");
-let profilePopupBtn = document.getElementById("profilePopupBtn");
-let placePopupBtn = document.getElementById("placePopupBtn");
+const editButtonProfile = document.querySelector(".profile__edit-button");
+const addButtonProfile = document.querySelector(".profile__add-button");
 
-let closeIconProfile = document.getElementById("closeIconProfile");
-let closeIconPlace = document.getElementById("closeIconPlace");
+const profilePopupBtn = document.getElementById("profilePopupBtn");
+const placePopupBtn = document.getElementById("placePopupBtn");
 
-let profilePopup = document.getElementById("profilePopup");
-let placePopup = document.getElementById("placePopup");
+const closeIconProfile = document.getElementById("closeIconProfile");
+const closeIconPlace = document.getElementById("closeIconPlace");
+const closeIconImage = document.getElementById("closeIconImage");
 
-let openedPopup = document.querySelector(".popup_opened");
-let nameProfile = document.querySelector(".profile__name");
-let aboutProfile = document.querySelector(".profile__about");
+const profilePopup = document.getElementById("profilePopup");
+const placePopup = document.getElementById("placePopup");
+const imagePopup = document.getElementById("imagePopup");
 
-let nameInput = document.getElementById("name");
-let aboutMeInput = document.getElementById("aboutMe");
+const openedPopup = document.querySelector(".popup_opened");
+const nameProfile = document.querySelector(".profile__name");
+const aboutProfile = document.querySelector(".profile__about");
 
-let placeNameInput = document.getElementById("placeName");
-let linkInput = document.getElementById("link");
+const nameInput = document.getElementById("name");
+const aboutMeInput = document.getElementById("aboutMe");
 
-let popup = document.querySelector(".popup");
+const placeNameInput = document.getElementById("placeName");
+const linkInput = document.getElementById("link");
+
+const popup = document.querySelector(".popup");
 
 const elementTemplate = document.querySelector("#element").content;
 const gallery = document.querySelector(".elements__card");
@@ -51,8 +54,7 @@ const initialCards = [
   },
 ];
 
-let places = [];
-
+// profile popup
 function openProfilePopup(evt) {
   evt.preventDefault();
   profilePopup.classList.add("popup_opened");
@@ -82,6 +84,8 @@ editButtonProfile.addEventListener("click", function (evt) {
 closeIconProfile.addEventListener("click", function (evt) {
   closeProfilePopup(evt);
 });
+
+// place popup
 
 function openPlacePopup(evt) {
   evt.preventDefault();
@@ -115,16 +119,25 @@ function renderElement(newObj) {
   const newElement = elementTemplate
     .querySelector(".elements__item")
     .cloneNode(true);
-  newElement.querySelector(".elements__photo").src = newObj.link;
-  newElement.querySelector(".elements__photo").alt = newObj.name;
+  let image = newElement.querySelector(".elements__photo");
+  image.src = newObj.link;
+  image.alt = newObj.name;
   newElement.querySelector(".elements__title").textContent = newObj.name;
+
+  image.addEventListener("click", function () {
+    imagePopup.classList.add("popup_opened");
+    imagePopup.getElementsByTagName("img")[0].src = newObj.link;
+    imagePopup.getElementsByTagName("img")[0].alt = newObj.name;
+    imagePopup.getElementsByTagName("figcaption")[0].textContent = newObj.name;
+  });
+
   let like = newElement.querySelector(".elements__like");
   like.addEventListener("click", function () {
-  like.classList.toggle("elements__like-active");
+    like.classList.toggle("elements__like-active");
   });
   let deletebutton = newElement.querySelector(".elements__delete");
   deletebutton.addEventListener("click", function () {
-  newElement.remove()
+    newElement.remove();
   });
   gallery.prepend(newElement);
 }
@@ -133,4 +146,11 @@ for (let place of initialCards) {
   renderElement(place);
 }
 
-
+// img-popup
+function closeImagePopup(evt) {
+  evt.preventDefault();
+  imagePopup.classList.remove("popup_opened");
+}
+closeIconImage.addEventListener("click", function (evt) {
+  closeImagePopup(evt);
+});
